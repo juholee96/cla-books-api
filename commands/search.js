eff1ef804864e1032d830243f7c3083607f3c865
Kevin Lee <juho.lee96@gmail.com>
2021-06-30 14:37:23 -0500

    added styling to the results

--- commands/search.js
+++ commands/search.js
@@ -1,5 +1,15 @@
 const yargs = require("yargs");
 const axios = require("axios");
+const chalk = require("chalk");
+const boxen = require("boxen");
+
+const boxenOptions = {
+  padding: 1,
+  margin: 1,
+  borderStyle: "round",
+  borderColor: "green",
+  backgroundColor: "#555555",
+};
 
 const search = async (keyword) => {
   try {
@@ -9,21 +19,23 @@ const search = async (keyword) => {
       .argv;
 
     if (options.search) {
-      console.log(`Searching for books about ${options.search}...`);
+      const msg = chalk.white.bold(`Searching for books about ${options.search}...
+      \n Here are 5 books related to your search: `);
+      const msgBox = boxen(msg, boxenOptions);
+      console.log(msgBox);
     }
     const results = await axios.get(
       `https://www.googleapis.com/books/v1/volumes?q=${options.search}&printType=books&startIndex=0&maxResults=5`
     );
     results.data.items.forEach((book) => {
-      //   console.log(book.volumeInfo);
-      console.log(`
-    ******************************************************
-        Title: ${book.volumeInfo.title}
-        Author(s): ${book.volumeInfo.authors}
-        Publisher: ${book.volumeInfo.publisher}
-        ISBN: ${book.volumeInfo.industryIdentifiers[1].identifier}
-    ******************************************************
-        `);
+      const bookInfo = chalk.green.bold(`
+      Title: ${book.volumeInfo.title}
+      Author(s): ${book.volumeInfo.authors}
+      Publisher: ${book.volumeInfo.publisher}
+      ISBN: ${book.volumeInfo.industryIdentifiers[1].identifier}`);
+
+      const bookCard = boxen(bookInfo, boxenOptions);
+      console.log(bookCard);
     });
   } catch (error) {
     console.error(error);
